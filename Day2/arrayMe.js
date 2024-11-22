38a44a302d88a32d9ce4eaef82c655bc6f616478
sundipthapa <lakhanmgr123@gmail.com>
2024-11-22 16:29:31 +0545

    :lipstick: Array in typescript

---
+++ Day2/arrayMe.js
@@ -0,0 +1,9 @@
+var superheros = [];
+superheros.push("Spiderman");
+var herosuper = [];
+herosuper.push(1);
+var allUsers = [];
+var Mlmodels = [
+    // [11,11,11,11],
+    ["hello", "world"]
+];
---
+++ Day2/arrayMe.ts
@@ -0,0 +1,18 @@
+const superheros: string[] = [];
+superheros.push("Spiderman");
+
+const herosuper: Array<number> = [];
+herosuper.push(1);
+
+type Users = {
+  name: string;
+  isActive: boolean;
+};
+const allUsers: Users[] = [];
+
+const Mlmodels: string[][] = [
+  // [11,11,11,11],
+  ["hello", "world"],
+];
+
+export {};
